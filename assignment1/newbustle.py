85bfa2e8008f8a934c144fce6fb70819e8ae2214
SIBI RAJA <sraja@college.harvard.edu>
2023-10-17 19:04:02 -0400

    initial working version of arithmeticDSL finished i think?

--- assignment1/newbustle.py
+++ assignment1/newbustle.py
@@ -3,19 +3,21 @@ from tqdm import tqdm
 
 # List of tasks, where each task is a list of IO examples.
 arithmetic_input_output_examples = [
-    # [([1,2], 3), ([2,3], 5), ([3,4], 7), ([4,5], 9)], # add 2 numbers
+    [([1,2], 3), ([2,3], 5), ([3,4], 7), ([4,5], 9)], # add 2 numbers
     # [([1,2], 2), ([2,3], 6), ([3,4], 12), ([4,5], 20)], # multiply 2 numbers
     # [([1,2], -1), ([2,3], -1), ([3,4], -1), ([4,6], -2)], # subtract 2 numbers
     # [([1,2], 0), ([6,3], 2), ([10,4], 2), ([49,7], 7)], # divide 2 numbers
-    # [([1,2], 4), ([2,3], 6), ([3,4], 8), ([4,5], 10)], # add 2 numbers and add 1 to result
-    [([1,2], 9), ([2,3], 17), ([3,4], 25), ([4,5], 33)], # add 2 numbers, multiply result by 4, and subtract 3
+    [([1,3], 8), ([2,4], 12), ([3,7], 20), ([4,8], 24)], # add 2 numbers and multiply result by 2
+    # [([1,3], 13), ([2,5], 25), ([3,7], 37), ([4,3], 23)], # add 2 numbers, multiply result by 4, and subtract 3
     # TODO: THE CORRECT EXPRESSION FOR THE LAST INPUT OUTPUT EXAMPLE IS NOT BEING CONSTRUCTED. INVESTIGATE THIS FURTHER
 
 ]
 
+programs_found = []
+
 # operations = ["ADD", "SUBTRACT", "MULTIPLY", "DIVIDE"]
-operations = [("ADD", 2), ("MULTIPLY", 2), ("SUBTRACT", 2), ("DIVIDE", 2)]
-# operations = [("ADD", 2), ("MULTIPLY", 2)]
+# operations = [("ADD", 2), ("MULTIPLY", 2), ("SUBTRACT", 2), ("DIVIDE", 2)]
+operations = [("ADD", 2), ("MULTIPLY", 2)]
 
 # def execute(op, *args):
 #     if op == "ADD":
@@ -52,24 +54,29 @@ def evaluate_expression(expr, input_mapping):
 
     try:
         temp = str(eval(expr))
-    except ZeroDivisionError:
+    except ZeroDivisionError: # TODO: CAN ALSO ADD OTHER ERROR CHECKS HERE RELEVEANT TO STRING DSL MAYBE?
         print("ZERO DIVISION ERROR")
         return "ERROR"
     
     # print("EVALUATED EXPRESSION: ", temp)
     return temp
 
-max_weight = 8
+max_weight = 5 # this represents the actual term weight for our desired expression
 
 # Now, for each task's examples, run the synthesis process.
 for task_examples in arithmetic_input_output_examples:
 
     E = {1: []}
+    results_seen = set()
+
     for io_example in task_examples:
         for element in io_example[0]:
             element = str(element)
             if ("CONST", element) not in E[1]:
                 E[1].append(("CONST", element))
+                curr_results = [str(element)] * len(task_examples)
+                curr_results = tuple(curr_results) # convert the list to a tuple since sets cannot contain lists
+                results_seen.add(curr_results)
     E[1].extend([("VAR", f"x{i}") for i in range(len(task_examples[0][0]))])  # Add the input variables
     # variable_set = set([f"x{i}" for i in range(len(task_examples[0][0]))])
 
@@ -85,17 +92,25 @@ for task_examples in arithmetic_input_output_examples:
         if ("CONST", str(i)) not in E[1]:
             E[1].append(("CONST", str(i)))
             args_to_weights[("CONST", str(i))] = 1
+            curr_results = [str(i)] * len(task_examples)
+            curr_results = tuple(curr_results)
+            results_seen.add(curr_results)
+
+    # print(results_seen)
+    # exit()
 
     # print(f"args_to_weights: {args_to_weights}\n")
     # exit()
 
     # for w in range(2, max_weight + 1):
     for w in tqdm(range(2, max_weight + 1)):
+        print("W is now: ", w)
         for operation_tuple in operations:
+            print("We are now considering the following operation: ", operation_tuple)
 
             operation = operation_tuple[0]
 
-            n_op_arity = operation_tuple[1]
+            n_op_arity = operation_tuple[1] # plug 
 
             A = []
             values_in_E = [item for sublist in E.values() for item in sublist]
@@ -109,7 +124,11 @@ for task_examples in arithmetic_input_output_examples:
             # print(f"permutations: {permutations}")
 
             for permutation in permutations:
+                for arg in permutation:
+                    if arg not in args_to_weights:
+                        print("WEIGHT NOT FOUND")
                 sum_of_weights = sum([args_to_weights[arg] for arg in permutation])
+                print("Current permutation: ", permutation, " has weight: ", sum_of_weights, " and we are looking for weight: ", w)
                 if sum_of_weights == w - 1:
                     A.append(permutation)
 
@@ -117,7 +136,22 @@ for task_examples in arithmetic_input_output_examples:
             # print(f"Possible combinations (A): {A}\n")
             # exit()
 
+            print("A: " , A)
+
             for arg_tuple in A:
+                print("Hi!")
+
+                # consider the case where we have already found a program satisfying the current task via an expression constructed from a previous arg_tuple.
+                # Then we want to break out of this loop and continue to the next task
+                answer_needed = []
+                for (input_values, output) in task_examples:
+                    answer_needed.append(output)
+                
+                if tuple(answer_needed) in results_seen:
+                    print("Hello")
+                    break
+
+
                 # print(f"arg_tuple: {arg_tuple}")
                 # exit()
                 # result = execute(operation, *(arg[1] for arg in arg_tuple))
@@ -171,18 +205,23 @@ for task_examples in arithmetic_input_output_examples:
                 # but only add the expression if it consists of only constants and variables
                 # expr_split = expr.split()
 
-                add_to_E = True
-                for i, letter in enumerate(expr):
-                    if letter.isdigit() and (i == 0 or expr[i - 1] != "x"): # this assume we will not have more than 9 variables. I think this is fair assumption as that will be take too long to search for and synthesize a program anyways
-                        add_to_E = False
-                        # print("Hello")
-                        # break
-                if add_to_E:
-                    if w not in E:
-                        E[w] = [("EXPR", expr)]
-                        args_to_weights[("EXPR", expr)] = w
+                # add_to_E = True
+                # for i in range(1, len(expr)):
+                #     letter = expr[i]
+                #     if letter.isdigit() and (expr[i-1].isdigit()):
+                #         add_to_E = False
+                #         break
+                # for i, letter in enumerate(expr):
+                #     if letter.isdigit() and (i == 0 or expr[i - 1] != "x"): # this assume we will not have more than 9 variables. I think this is fair assumption as that will be take too long to search for and synthesize a program anyways
+                #         add_to_E = False
+                #         print("Hello")
+                #         break
+                # if add_to_E:
+                #     if w not in E:
+                #         E[w] = [("EXPR", expr)]
+                #         args_to_weights[("EXPR", expr)] = w
 
-                        # print("JUST ADDED: ", expr, "with weight: ", w, "\n")
+                #         print("JUST ADDED: ", expr, "with weight: ", w, "\n")
 
                 # if expr == "((x0 + x1) + 1)":
                 #     if expr in 
@@ -193,6 +232,10 @@ for task_examples in arithmetic_input_output_examples:
                 # if expr == "(x0 + x1)":
                 #     print("FOUND IT")
                     # exit()
+
+
+                # Keep a list of what the current expression evaluates to for each example in the task.
+                curr_results = []
 
 
                 all_correct = True
@@ -210,24 +253,60 @@ for task_examples in arithmetic_input_output_examples:
                         all_correct = False
                         break
 
-                    
+                    curr_results.append(expr_result)
 
                     if expr_result != str(output):
                         # if expr == "(x0 + x1)":
                         #     print("Something went wrong")
                         #     exit()
                         all_correct = False
-                        break
+                        # break --> don't break here, because we want to add the expression to E even if it is incorrect for some examples in order to correctly get all the results of an expression before checking if we need to add it to E
+
+                print(f"THE CURRENT EXPRESSION {expr} HAD THE FOLLOWING RESULTS: ", curr_results, "for task: ", task_examples, "with weight: ", w, "\n")
+                # exit()
+
                 
-                if all_correct:
+
+                
+                if len(curr_results) == len(task_examples):
+                    print("Hello")
+                    if tuple(curr_results) not in results_seen:
+                        results_seen.add(tuple(curr_results))
+
+                        if w not in E:
+                            E[w] = []
+                        
+                        E[w].append(("EXPR", expr))
+                        args_to_weights[("EXPR", expr)] = w
+
+                        print("JUST ADDED THE SUBEXPRESSION: ", expr, "with weight: ", w, "\n")
+                    
+                    else:
+                        print("WE HAVE ALREADY ACHIEVED THESE SETS OF RESULTS VIA ANOTHER EXPRESSION \n")
+                        all_correct = False # set this to false to indicate that we shouldn't add this program to our global bank
+
+
+                # only add a program to our global answer bank if it is the first program to satisfy the input output examples. otherwise, continue searching for the next program
+                # --> we take care of this at the top of loop
+
+                # only consider adding the current expression if it has the correct number of results in curr_results (otherwise it errored out on at least 1 example, so its invalid) and if all results are correct
+                if all_correct and len(curr_results) == len(task_examples):
                     # print(f"Found a program that fits the task: {expr}")
                     print("This program works: ", expr, "for task: ", task_examples, "with weight: ", w, "\n")
-                    # exit()
+                    programs_found.append("This program works: " + expr + "for task: " + str(task_examples) + "with weight: " + str(w) + "\n")
+
+                if len(programs_found) == len(arithmetic_input_output_examples):
+                    print("=====================================")
+                    print("WE HAVE FOUND THE FOLLOWING PROGRAMS: ")
+                    for program in programs_found:
+                        print(program)
+                    print("DONE")
+                    exit()
 
                 # if expr == "(x0 + x1)":
                 #     print("Something went wrong")
 
-print("DONE")
+print("If we reached this point, we did not synthesize programs :(")
 
 
 
